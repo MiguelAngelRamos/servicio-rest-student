0c13ece86be5d6d9cdc3c728dccb429d521312ab
Miguel Ramos <return.infox@gmail.com>
2025-08-14 19:49:02 -0400

    primmer commit

---
+++ src/main/java/cl/kibernumacademy/academy/AcademyApplication.java
@@ -0,0 +1,13 @@
+package cl.kibernumacademy.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyApplication.class, args);
+	}
+
+}
