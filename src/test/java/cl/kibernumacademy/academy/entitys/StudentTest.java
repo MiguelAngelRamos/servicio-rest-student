ebcb4a00d9261e370e10662cf02f4b6925b58687
Miguel Ramos <return.infox@gmail.com>
2025-08-19 21:17:08 -0400

    test para el POJO

--- src/test/java/cl/kibernumacademy/academy/entitys/StudentTest.java
+++ src/test/java/cl/kibernumacademy/academy/entitys/StudentTest.java
@@ -1,5 +1,36 @@
 package cl.kibernumacademy.academy.entitys;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class StudentTest {
   
+  private Student student;
+
+  @BeforeEach
+  public void setUp() {
+    student = new Student("James", "Gosling", "james.gosling@java.com");
+  }
+
+  @Test
+  public void shoudSetName() {
+    student.setName("Richard");
+    assertEquals("Richard", student.getName());
+  }
+
+  @Test
+  public void shouldSetLastName() {
+    student.setLastname("Stallman");
+    assertEquals("Stallman", student.getLastname());
+  }
+
+  @Test
+  public void shouldSetEmail() {
+    student.setEmail("richard.stallman@gnu.com");
+    assertEquals("richard.stallman@gnu.com", student.getEmail());
+  }
+
+
 }
